b531ac8482149d9eca9f2edd0ed2022d29270405
Ilgaz Turhan <ilgaz@Ilgaz-MacBook-Air.local>
2022-12-20 13:03:40 +0300

    selam

---
+++ main.js
@@ -0,0 +1,9 @@
+const kutu = document.getElementById("tekKutu")
+
+kutu.addEventListener("mouseover", () => {
+    kutu.style.backgroundColor = "purple"
+    
+})
+kutu.addEventListener("mouseleave", () => {
+    kutu.style.backgroundColor = "red"
+})
